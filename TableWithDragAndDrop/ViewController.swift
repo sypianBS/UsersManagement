c279c4d72c46131d6eeb31168e63ac769e19bb20
sypianBS <sypianbs@gmail.com>
2022-03-12 16:50:33 +0100

    init files

---
+++ TableWithDragAndDrop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableWithDragAndDrop
+//
+//  Created by Beniamin on 12.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
