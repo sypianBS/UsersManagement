1c2d8880a2b8bfe665ac079e13986b7b6f9b6cb9
sypianBS <sypianbs@gmail.com>
2022-03-12 17:15:42 +0100

    add tab bar controller and empty tabs

--- TableWithDragAndDrop/ViewController.swift
+++ TableWithDragAndDrop/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+import UIKit
+class ViewController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let favoritesTab = FavoritesViewController()
+        let favoritesTabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "star"), selectedImage: UIImage(systemName: "star.fill"))
+        
+        favoritesTab.tabBarItem = favoritesTabBarItem
+        
+        let mainListTab = MainListViewController()
+        let mainListTabBarItem = UITabBarItem(title: "List", image: UIImage(systemName: "list.bullet.rectangle.portrait"), selectedImage: UIImage(systemName: "list.bullet.rectangle.portrait.fill"))
+        
+        mainListTab.tabBarItem = mainListTabBarItem
+        
+        let settingsTab = SettingsViewController()
+        let settingsTabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape"))
+        
+        settingsTab.tabBarItem = settingsTabBarItem
+        
+        
+        self.viewControllers = [favoritesTab, mainListTab, settingsTab]
+    }
+        
 }
-
