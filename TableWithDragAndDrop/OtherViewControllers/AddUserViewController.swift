5878a64807a55aca6ec0869717ee6c53b1e07d85
sypianBS <sypianbs@gmail.com>
2022-03-26 08:03:43 +0100

    add first name, last name and email input

--- TableWithDragAndDrop/OtherViewControllers/AddUserViewController.swift
+++ TableWithDragAndDrop/OtherViewControllers/AddUserViewController.swift
@@ -29,26 +29,42 @@ class AddUserViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        let addFirstNameLabel = UILabel()
-        addFirstNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        addFirstNameLabel.text = "First name"
-        
-        let addFirstNameTextField = UITextField()
-        addFirstNameTextField.translatesAutoresizingMaskIntoConstraints = false
-        addFirstNameTextField.text = nil
-        addFirstNameTextField.placeholder = "e.g. John"
-        
-        let firstNameStackView = UIStackView()
-        firstNameStackView.axis = .horizontal
-        firstNameStackView.spacing = 16
-        firstNameStackView.translatesAutoresizingMaskIntoConstraints = false
-        firstNameStackView.addArrangedSubview(addFirstNameLabel)
-        firstNameStackView.addArrangedSubview(addFirstNameTextField)
-
-        newUserStackView.addArrangedSubview(firstNameStackView)
         view.addSubview(sheetTitleLabel)
         sheetTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         sheetTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+        view.addSubview(newUserStackView)
+        newUserStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        newUserStackView.topAnchor.constraint(equalTo: sheetTitleLabel.bottomAnchor, constant: 24).isActive = true
+        
+        let firstNameInput = makeLabelTextInputStackView(labelName: "First name", textFieldPlaceholder: "e.g. John")
+        let lastName = makeLabelTextInputStackView(labelName: "Last name", textFieldPlaceholder: "e.g. Doe")
+        let email = makeLabelTextInputStackView(labelName: "Email", textFieldPlaceholder: "e.g. john.doe@test.com")
+        
+        newUserStackView.addArrangedSubview(firstNameInput)
+        newUserStackView.addArrangedSubview(lastName)
+        newUserStackView.addArrangedSubview(email)
+    }
+    
+    private func makeLabelTextInputStackView(labelName: String, textFieldPlaceholder: String) -> UIStackView {
+        let labelTextInputStackView = UIStackView()
+        labelTextInputStackView.translatesAutoresizingMaskIntoConstraints = false
+        labelTextInputStackView.axis = .horizontal
+        labelTextInputStackView.spacing = 16
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = labelName + ":"
+        label.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = nil
+        textField.placeholder = textFieldPlaceholder
+        
+        labelTextInputStackView.addArrangedSubview(label)
+        labelTextInputStackView.addArrangedSubview(textField)
+        
+        return labelTextInputStackView
     }
 
 }
